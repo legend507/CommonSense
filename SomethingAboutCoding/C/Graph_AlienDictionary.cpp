5eaaa4208ad89108b72ba3c1cc5b00557b9a4be6
legend507 <chenfeiyu507@gmail.com>
2018-03-06 13:44:11 +0900

    add geekforgeeks questions

---
+++ SomethingAboutCoding/C/Graph_AlienDictionary.cpp
@@ -0,0 +1,152 @@
+/*
+ Given a sorted dictionary (array of words) of an alien language, find order of characters in the language.
+ 
+ Examples:
+ 
+ Input:  words[] = {"baa", "abcd", "abca", "cab", "cad"}
+ Output: Order of characters is 'b', 'd', 'a', 'c'
+ Note that words are sorted and in the given language "baa"
+ comes before "abcd", therefore 'b' is before 'a' in output.
+ Similarly we can find other orders.
+ 
+ Input:  words[] = {"caa", "aaa", "aab"}
+ Output: Order of characters is 'c', 'a', 'b'
+
+以下解法非我原创，不过非常有参考价值，特别是graph的创建方法
+ 解释一下思想：
+ 0. check不同字母个数，construct一个graph，根据不同字母个数建立节点
+ 1. 比较word[i]和word[i+1]的每一对应letter，如果看到一个不同，就在graph的那两个定点上add edge，edge的方向是 先出现的letter -> 后出现的letter
+ (通过以上两步可以得到一个包含所有信息的graph)
+ 2. 遍历graph，发现最不重要的node，消除，然后重复	<- 这个递归方法很有参考价值
+ */
+
+#include <vector>
+#include <iostream>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <stack>
+using namespace std;
+
+// A BST node
+struct node
+{
+    int data;
+    node* left, *right;
+    
+    node(int x) : data(x), left(NULL), right(NULL) {}
+};
+
+class Graph {
+public:
+    int v;
+    list<int> *adj;
+    
+    Graph(int v) {
+        this->v = v;
+        adj = new list<int>[v];
+    }
+    
+	// add an edge v -> w (the edge has direction!!)
+    void addEdge(int v, int w) {
+        adj[v].push_back(w);
+    }
+    
+	void topologicalSort() {
+		stack<int> s;
+		bool *visited = new bool[v];
+		for (int i = 0; i < v; i++)
+			visited[i] = false;
+
+		// recursively push all nodes into stack
+		for (int i = 0; i < v; i++) {
+			if (visited[i] == false) {
+				topologicalSortUtil(i, visited, s);
+			}
+		}
+
+		// print
+		while (!s.empty()) {
+			cout << (char)('a' + s.top()) << "->";
+			s.pop();
+		}
+		cout << endl;
+	}
+
+	/*
+	topological sort func, this func removes the "least significant node" (the node does not pointing to any other node)
+	and then remove the next least significant node, ..., until there is only one node left
+	*/
+    void topologicalSortUtil(int i, bool visited[], stack<int>& s) {
+		visited[i] = true;
+
+		for (auto itr = adj[i].begin(); itr != adj[i].end(); itr++)
+			// if hasn't visited yet
+			if (!visited[*itr])
+				topologicalSortUtil(*itr, visited, s);	
+		
+		// if i has no edge pointing to other node
+		s.push(i);
+    }
+    
+    void printGraph() {
+        for(int i = 0; i < v; i++) {
+			cout << "[" << i << "] -> ";
+            for(auto itr=adj[i].begin(); itr != adj[i].end(); itr++) {
+                cout << *itr << " ";
+            }
+            cout << endl;
+        }
+    }
+    
+};
+
+class AlienDict {
+public:
+    void process(vector<string>& words) {
+
+		// 1st, find all distinctive letter 
+        unordered_map<char, int> record;
+        for(auto oneWord:words) {
+            for(int i = 0; i < oneWord.size(); i++) {
+                record[oneWord[i]] = (int)oneWord[i];
+            }
+        }
+        
+        int allLetters = (int)record.size();
+        Graph g(allLetters);
+        
+		// 2nd, compare every word with next word, add edges
+        for(int i = 0; i < words.size()-1; i++) {
+            string word1 = words[i];
+            string word2 = words[i+1];
+            
+            for(int j = 0; j < min(word1.size(), word2.size()); j++) {
+                if(word1[j] != word2[j]) {
+					// if adj[2] = 1 (2 means 'c', 1 means 'b'), then c is prior than b
+                    cout << "adj[" << word1[j]-'a' << "]=" <<word2[j]-'a' << endl;
+                    g.addEdge(word1[j]-'a', word2[j]-'a');
+                    break;
+                }
+            }
+        }
+		g.topologicalSort();
+    }
+};
+
+int main() {
+    
+    vector<string> words = { "caa",
+        "aaa",
+        "aab" };
+    AlienDict ad;
+    ad.process(words);
+    
+	system("pause");
+    return 0;
+}
---
+++ SomethingAboutCoding/C/Tree_hasIdenticalSubtree.cpp
@@ -0,0 +1,148 @@
+/*
+下面的Class可以check
+1. 两个Tree是否完全相同
+2. 一个Tree是否是另一个Tree的Subtree
+3. 一个Tree中是否包含相同subtree
+ */
+
+#include <vector>
+#include <iostream>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <stack>
+using namespace std;
+
+// A BST node
+//template <typename T>
+//struct TreeNode
+//{
+//    T data;
+//	TreeNode* left, *right;
+//    
+//	TreeNode(T x) : data(x), left(NULL), right(NULL) {}
+//};
+
+struct TreeNode
+{
+	char data;
+	TreeNode* left, *right;
+
+	TreeNode(char x) : data(x), left(NULL), right(NULL) {}
+};
+
+class TreeChecker {
+	/*
+	this class provide a way to check 
+		if two trees are identical
+		if one tree is a subtree of another
+	*/
+
+public:
+	/*
+	Recursively check if two trees are identical
+	*/
+	bool areIdentical(TreeNode* root1, TreeNode* root2) {
+		// base case
+		if (root1 == NULL && root2 == NULL)	return true;
+		if (root1 == NULL || root2 == NULL) return false;
+
+		// recurse case
+		//	check if data of both root & data of right child & data of left child are the same
+		return (
+			root1->data == root2->data
+			&& areIdentical(root1->left, root2->left)
+			&& areIdentical(root1->right, root2->right)
+			);
+	}
+
+	/*
+	check if smallT is a subtree of bigT
+	*/
+	bool isSubtree(TreeNode* bigT, TreeNode* smallT) {
+		// base case
+		if (smallT == NULL)	return true;
+		if (bigT == NULL) return false;
+
+		if (areIdentical(bigT, smallT))	return true;
+
+		// recurse case
+		return (
+			isSubtree(bigT->left, smallT)
+			|| isSubtree(bigT->right, smallT)
+			);
+	}
+
+	/*
+	Does a tree contains identical subtrees (size of subtree >= 2)?
+	subtree的定义是，从tree的某一点开始，到所有leaf为止；并非任取tree的一段
+	e.g.,
+	Input :  Binary Tree 
+               A
+             /    \ 
+           B        C
+         /   \       \    
+        D     E       B     
+                     /  \    
+                    D    E
+	Output : Yes
+	*/
+	// 下面的func，如果有identical subtree，则返回“”，如果没有；则返回tree的serial （e.g., ABD$$E$$C$BD$$ES$$$）
+	string hasIdenticalSubtree(TreeNode* root) {
+		unordered_set<string> subtrees;
+
+		return hasIdenticalSubtreeUtil(root, subtrees);
+	}
+	string hasIdenticalSubtreeUtil(TreeNode* root, unordered_set<string>& subtrees) {
+		string s = "";
+
+		// base case
+		if (root == NULL)	return (s + "$");
+		
+		// recurse case
+		string leftStr = hasIdenticalSubtreeUtil(root->left, subtrees);
+		if (leftStr.compare(s) == 0)	return s;
+		string rightStr = hasIdenticalSubtreeUtil(root->right, subtrees);
+		if (rightStr.compare(s) == 0)	return s;
+
+		s = s + root->data + leftStr + rightStr;
+
+		// store all subtrees in subtrees hashtable, paramete 3 means a signle node (e.g., A$$) is not a subtree
+		if (s.length() > 3
+			&& subtrees.find(s) != subtrees.end())
+			return "";
+
+		cout << s << endl;
+
+		// 存储这个tree的所有subtree
+		subtrees.insert(s);
+
+		return s;
+	}
+};
+
+
+int main() {
+	TreeNode *root = new TreeNode('A');
+	root->left = new TreeNode('B');
+	root->right = new TreeNode('C');
+	root->left->left = new TreeNode('D');
+	root->left->right = new TreeNode('E');
+	root->right->right = new TreeNode('B');
+	root->right->right->right = new TreeNode('E');
+	root->right->right->left = new TreeNode('D');
+
+	root->right->right->right->left = new TreeNode('S');
+
+	TreeChecker tc;
+	cout << "---" << tc.hasIdenticalSubtree(root);
+
+	system("pause");
+    return 0;
+}
