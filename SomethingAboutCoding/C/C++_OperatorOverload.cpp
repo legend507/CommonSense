fdcc1d5a824ed7162bdcdec360800d88efc63702
legend507 <chenfeiyu507@gmail.com>
2018-03-30 17:01:53 +0900

    add knowledge about OSI, add string compare .cpp

--- SomethingAboutCoding/C/C++_OperatorOverload.cpp
+++ SomethingAboutCoding/C/C++_OperatorOverload.cpp
@@ -1,4 +1,4 @@
-template <typename T>
+﻿template <typename T>
 struct interval {
 	T start;
 	T end;
@@ -9,7 +9,7 @@ struct interval {
 		stream << "{" << obj.start << "," << obj.end << "}";
 		return stream;
 	};
-	// compare func
+	// compare func, is i1 smaller than i2? if used in sort, result is ascending
 	static bool compare(interval<T>& i1, interval<T>& i2) {
 		return (i1.start < i2.start);
 	}
@@ -30,4 +30,57 @@ struct cell {
 
 	cell() :parent_i(-1), parent_j(-1), f(FLT_MAX), g(FLT_MAX), h(FLT_MAX) {};
 };
+
+/*
+以下是一个把Person的名字按照字典序拍列的compare func
+注意，如果两个人名完全一样，则 compare(p1, p2) == true, 并且 compare(p2, p1) == true
+这时候会有 runtime error
+*/
+// a structure of TreeNode
+struct Person {
+	string familyName;
+	string givenName;
+
+	Person(string fn, string gn) :familyName(fn), givenName(gn) {}
+
+	/*is p1 small than p2*/
+	static bool compare(Person& p1, Person& p2) {
+		string p1FullName = p1.familyName + " " + p1.givenName;
+		string p2FullName = p2.familyName + " " + p2.givenName;
+
+		int i = 0;
+		int j = 0;
+		for (;
+			i < p1FullName.size() && j < p2FullName.size();
+			i++, j++) {
+			// p1:a, p2:a
+			if (p1FullName[i] == p2FullName[j])	continue;
+			// p1:b, p2:a
+			if (p1FullName[i] > p2FullName[j]) return false;
+			else return true;
+		}
+
+		// one reached the end
+		if (i == p1FullName.size())	return true;
+		else return false;
+	}
+
+	friend ostream& operator << (ostream& os, Person& p) {
+		os << p.familyName << " " << p.givenName;
+		return os;
+	}
+};
+
+
+int main() {
+
+	vector<Person> names = { Person("ab", "cd"), Person("a", "bcd"), Person("ac", "bd"), Person("ab", "c"), Person("e", "l") };
+
+	sort(names.begin(), names.end(), Person::compare);
+	for (auto one : names)	cout << one << endl;
+
+
+	system("pause");
+	return 0;
+}
 
