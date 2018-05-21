6db93b96a82b0d1f5d55b762e1b69a22833354c6
legend507 <chenfeiyu507@gmail.com>
2018-05-21 19:32:38 +0900

    Updated is binary search tree .cpp

--- SomethingAboutCoding/C/tree_isBinarySearchTree.cpp
+++ SomethingAboutCoding/C/tree_isBinarySearchTree.cpp
@@ -37,38 +37,55 @@ public:
 		if (root == NULL)	return true;
 
 		// recurse case
-		//	false if the max of the left is > than root->val
+		//	false if the max of the left is > root->val
 		if (root->left != NULL && findMaxValue(root->left) > root->val)		return false;
 
-		//	false if the min of the right is < than root->val
+		//	false if the min of the right is < root->val
 		if (root->right != NULL && findMinValue(root->right) < root->val)	return false;
 
 		//	false if left or right is not a BST
 		if (!isBST(root->left) || !isBST(root->right))	return false;
 
 		return true;
+	}
+	/*Efficient*/
+	bool isBST_efficient(TreeNode* root) {
 
 	}
 
+
+	/*2 utility functions, to find max and min value of current binary tree*/
 	int findMaxValue(TreeNode* root) {
 		// base case
 		if (root == NULL)	return INT_MIN;
 
 		// recurse case
-		return max(root->val, findMaxValue(root->left), findMaxValue(root->right));
+		return max(root->val, max(findMaxValue(root->left), findMaxValue(root->right)));
 	}
 	int findMinValue(TreeNode* root) {
 		// base case
 		if (root == NULL)	return INT_MAX;
 
 		// recurse case
-		return min(root->val, findMaxValue(root->left), findMaxValue(root->right));
+		return min(root->val, min(findMinValue(root->left), findMinValue(root->right)));
 	}
 };
 
 int main()
 {
 	Solution s;
+
+	TreeNode *root = new TreeNode(4);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(5);
+	root->left->left = new TreeNode(1);
+	root->left->right = new TreeNode(3);
+
+	cout << s.findMaxValue(root) << endl;
+	cout << s.findMinValue(root) << endl;
+
+	if (s.isBST(root)) cout << "YES";
+	else cout << "NO";
 
 	system("pause");
 	return 0;
