3653d5c292feb3bc7910c3ecd51262fb9944f29e
legend507 <chenfeiyu507@gmail.com>
2018-06-10 23:14:53 +0900

    reviewing knowledges

--- SomethingAboutCoding/C/algorithm_OverlapProblem.cpp
+++ SomethingAboutCoding/C/algorithm_OverlapProblem.cpp
@@ -120,3 +120,94 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+/*
+Find the point where maximum intervals overlap
+Consider a big party where a log register for guest’s entry and exit times is maintained. Find the time at which there are maximum guests in the party. Note that entries in register are not in any order.
+
+Example :
+
+Input: arrl[] = {1, 2, 9, 5, 5}
+exit[] = {4, 5, 12, 9, 12}
+First guest in array arrives at 1 and leaves at 4,
+second guest arrives at 2 and leaves at 5, and so on.
+
+Output: 5
+There are maximum 3 guests at time 5.
+*/
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+/*
+An Efficient Solution is to use sorting n O(nLogn) time. The idea is to consider all events (all arrivals and exits) in sorted order. Once we have all events in sorted order, we can trace the number of guests at any time keeping track of guests that have arrived, but not exited.
+arr[]  = {1, 2, 10, 5, 5}
+dep[]  = {4, 5, 12, 9, 12}
+
+Below are all events sorted by time.  Note that in sorting, if two
+events have same time, then arrival is preferred over exit.
+Time     Event Type         Total Number of Guests Present
+------------------------------------------------------------
+1        Arrival                  1
+2        Arrival                  2
+4        Exit                     1
+5        Arrival                  2
+5        Arrival                  3    // Max Guests
+5        Exit                     2
+9        Exit                     1
+10       Arrival                  2
+12       Exit                     1
+12       Exit                     0
+
+*/
+class Solution {
+public:
+	int findMaxGuests(vector<int>& in, vector<int>& out) {
+		// 还是先排序，不关心某个具体的人道in或out时间，只关注在某个时间点 club里有多少人
+		sort(in.begin(), in.end());
+		sort(out.begin(), out.end());
+
+		int allGuest = 1, ret = 1, time = in[0];
+		int inPtr = 1, outPtr = 0;
+
+		while (inPtr < in.size() && outPtr < out.size()) {
+			// think about why <= instead of <
+			if (in[inPtr] <= out[outPtr]) {
+				allGuest++;
+
+				if (allGuest > ret) {
+					ret = allGuest;
+					time = in[inPtr];
+				}
+				inPtr++;
+			} 
+			else {
+				allGuest--;
+				outPtr++;
+			}
+		}
+
+		return ret;
+	}
+
+};
+
+
+int main() {
+	Solution s;
+	vector<int> nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+
+
+
+	system("pause");
+	return 0;
+
+}
