51d13c447a2cd7375ad53619045ae0d403ff3f70
legend507 <chenfeiyu0620@gmail.com>
2018-07-21 08:08:43 +0900

    added checklist to investing, trying to fix http.py

--- httpsTest.py
+++ httpsTest.py
@@ -7,10 +7,10 @@ http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()    )
 my_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-refer = 'https://finance.yahoo.com/quote/%5EN225/history?period1=-157453200&period2=1530802800&interval=1d&filter=history&frequency=1d'
+referer = 'https://finance.yahoo.com/quote/%5EN225/history?period1=-157453200&period2=1530802800&interval=1d&filter=history&frequency=1d'
 
 r = http.request('GET', 
-                 refer)
+                 referer)
 
 print(r.headers['set-cookie'])
 
