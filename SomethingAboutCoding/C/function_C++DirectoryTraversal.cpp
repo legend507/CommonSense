808bc46c230cfad2c0b7c8cde15f4eccb175d4fb
legend507 <chenfeiyu507@gmail.com>
2018-09-10 18:08:07 +0900

    improve C++ directory traversal
    - code
    imrproved C++ directory traversal by adding string type convert

--- SomethingAboutCoding/C/function_C++DirectoryTraversal.cpp
+++ SomethingAboutCoding/C/function_C++DirectoryTraversal.cpp
@@ -12,8 +12,13 @@ int main(int argc, char *argv[])
 	fs::create_directories("sandbox/a/b");
 	std::ofstream("sandbox/file1.txt");
 	std::ofstream("sandbox/file2.txt");
-	for (auto& p : fs::directory_iterator("sandbox"))
+	for (auto& p : fs::directory_iterator("sandbox")) {
 		std::cout << p << std::endl;
+
+		// p is not in string format, to convert to string, use the following line
+		std::string fullPath = p.path().string();
+
+	}
 	fs::remove_all("sandbox");
 
 	system("pause");
