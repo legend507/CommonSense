f336d6b4fa6e678afa4421e78f56f30a677d3a1f
legend507 <chenfeiyu507@gmail.com>
2018-09-25 15:07:31 +0900

    add code
    - HTML+JS
    Add example of Google Map

---
+++ SomethingAboutCoding/GoogleMapDemp(HTML+JS)/ReadMe.txt
@@ -0,0 +1,2 @@
+a webpage (index.html) that displays Google Map, with a ballon on it.
+When the ballon is clicked, an info window pops up with Mr. Bean waving at you.
---
+++ SomethingAboutCoding/GoogleMapDemp(HTML+JS)/script.js
@@ -0,0 +1,33 @@
+function myMap() {
+    /*--------------------Map initialization---------------------------*/
+    // mapProp is setting up properties
+    var mapProp= {
+      center:new google.maps.LatLng(35.00000,139.00000),    // define zoom center in initilization
+      zoom:6,                                               // define zoom level (far 0 ~ 8 near)
+    };
+    // get element by name
+    var mapCanvas = document.getElementById("googleMap");
+    // call Google Map API, with elements and mapProp properties
+    var map=new google.maps.Map(mapCanvas, mapProp);
+
+    /*----------------------Put balloons on Oil bases------------------------*/
+    var base1 = new google.maps.LatLng(35.457241, 139.697550);
+    var marker_base1 = new google.maps.Marker({
+        position: base1,
+        //animation: google.maps.Animation.BOUNCE,          // this animation keeps bouncing...
+    });
+    marker_base1.setMap(map);
+
+    /*----------------------Prepare info window on a balloon------------------------*/
+    // for base1's info window content
+    var info_base1 = "<img src='img/MrBeans.gif'><br/><b>Base1</b><br/>123 Address<br/> City,Country";
+    var infoWindow_base1 = new google.maps.InfoWindow({
+        content: info_base1,
+    });
+
+    /*--------------Event listener, display info window when a balloon is clicked---------*/
+    google.maps.event.addListener(marker_base1, 'click', function(){
+        infoWindow_base1.open(map, marker_base1);
+    });
+
+}
